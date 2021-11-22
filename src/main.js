3c567199a605656a763a9f584bf02d4ed114a2b6
RissaDwi <rissadwiharprilenii@gmail.com>
2021-11-22 14:54:30 +0700

    masih bingung

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import PortalVue from "portal-vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8080/';
+
+Vue.use(PortalVue);
 Vue.config.productionTip = false;
 
 new Vue({
